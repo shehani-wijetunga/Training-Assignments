2fade335d33f04c13efbe688fb8d9da3c1c2ecbb
shehani-wijetunga <shehaniwijetunga96@gmail.com>
2021-02-15 06:13:05 +0530

    first commit

---
+++ video_15_service_chaining/customer-service/src/main/java/com/shehani/rentcloud/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,16 @@
+package com.shehani.rentcloud.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.shehani.rentcloud.model.customer")
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
